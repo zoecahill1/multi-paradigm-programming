042890163dee7f4ba62cd040c7cc5dc52c3160df
Zoe <zcahill90@gmail.com>
2019-10-13 22:11:09 +0100

    edited prints for formatting and shoppinglist() for creation

--- shopAssignment.c
+++ shopAssignment.c
@@ -10,6 +10,7 @@
 // https://stackoverflow.com/questions/28654792/what-do-i-need-to-do-so-the-function-isspace-work-in-c
 #include <ctype.h>
 
+
 // A struct is a type of data we define ourselves
 struct Product {
 	// using a pointer
@@ -34,6 +35,7 @@ struct Shop {
 struct Customer {
 	char* name;
 	double budget;
+	//memory
 	struct ProductStock shoppingList[10];
 	int index;
 };
@@ -41,21 +43,29 @@ struct Customer {
 void printProduct(struct Product p)
 {
 	printf("PRODUCT NAME: %s \nPRODUCT PRICE: %.2f\n", p.name, p.price);
-	printf("-------------\n");
+	//printf("-------------\n");
 }
 
 void printCustomer(struct Customer c)
 {
+	printf("\n\n-------------------\nCUSTOMER\n---------------------\n");
 	printf("CUSTOMER NAME: %s \nCUSTOMER BUDGET: %.2f\n", c.name, c.budget);
 
 	printf("-------------\n");
+	
+	double total = 0;
 	for(int i = 0; i < c.index; i++)
 	{
 		printProduct(c.shoppingList[i].product);
 		printf("%s ORDERS %d OF ABOVE PRODUCT\n", c.name, c.shoppingList[i].quantity);
 		double cost = c.shoppingList[i].quantity * c.shoppingList[i].product.price; 
 		printf("The cost to %s will be €%.2f\n\n", c.name, cost);
+		
+		total+=cost;
 	}
+	
+	printf("TOTAL: \t\t%.2f\n", total);
+	printf("REMAINING: \t%.2f\n", c.budget-total);
 	
 }
 
@@ -77,8 +87,6 @@ struct Shop createAndStockShop()
 
 	// &len and fp - read to end of file
     while ((read = getline(&line, &len, fp)) != -1) {
-        // printf("Retrieved line of length %zu:\n", read);
-        // printf("%s IS A LINE", line);
 		
 		// checking that cash has a value
 		if (strstr(line,"cash") != NULL){
@@ -145,51 +153,67 @@ const char * stripLine(char *textStr){
 
 struct Customer createShoppingList(char *csvfile){
 	
+	//https://ubuntuforums.org/showthread.php?t=1103327
 	struct Customer customer = { 0 };
 	FILE * fp;
 	char * line = NULL;
 	size_t length = 0;
 	ssize_t read;
+	//https://www.javatpoint.com/c-array
+	char *trackers[3];
 	
 	fp = fopen(csvfile, "r");
-	//if (fp == NULL)
-		//exit(EXIT_FAILURE);
+	if (fp == NULL)
+		exit(EXIT_FAILURE);
 	
+	// read csv line by line
 	while ((read = getline(&line, &length, fp)) != -1){
+		// reset count index
+		int i=0; 
 		
+		// read until see ,
 		char *n = strtok(line, ",");
-		char *b = strtok(NULL, ",");
-		char *name = malloc(sizeof(char) * 25);
-		char *budg = malloc(sizeof(char) * 25);
 		
-		//strcpy(name, n);
-		//strcpy(budg, b);
-		//name = stripNewline(n);
-		//budg = stripNewline(b);
-		
-		strcpy(name, stripLine(n));
-		strcpy(budg, stripLine(b));
-		
-		//if customer name is returned then do this
-		if (strstr(name, "Name") != NULL|strstr(name, "name") != NULL){
-			//printf("%s:\t%s\n", n, b);
-			customer.name = budg;
+		// read until null occurs
+		while (n != NULL){
+			// assign memory for count
+			char *tracker = malloc(sizeof(char) * 25);
+			// copies to count after formatting
+			strcpy(tracker, stripLine(n));
+			// add all to array
+			trackers[i++]=tracker;
+			//read until next null
+			n = strtok(NULL, ",");
 		}
 		
-		//otherwise if budget is returned do this
-		else if (strstr(name, "Budget") != NULL|strstr(name, "budget") != NULL){
-			double budget = atof(budg);
-			//printf("%s:\t%.2f\n", name ,budg);
+		// if customer name is returned then do this
+		if (strstr(trackers[0], "name") != NULL){
+			// add name to the customer struct
+			customer.name = trackers[1];
+			
+		}
+		
+		// otherwise if budget is returned do this
+		else if (strstr(trackers[0], "budget") != NULL){
+			
+			// atof casts to float
+			double budget = atof(trackers[1]);
+			// add to customer struct
 			customer.budget = budget;
 		}
-		//otherwise add the shopping list quantity to the customer is a strctured way
-		//and increment the index
 		
-		else {
-			int n = atoi(budg);
-			//printf("%s:\t%i\n", n, b);
-			struct Product product = { name };
-			struct ProductStock shopList = { product, n };
+		// adds the shopping list amounts to product structs
+		else{
+			// atoi casts to int. no of items
+			int n = atoi(trackers[2]);
+			// atof casts to float
+			double cost = atof(trackers[1]);
+			
+			// instance of product with name from array
+			struct Product product = {trackers[0], cost};
+			// add product and amount to list
+			struct ProductStock shopList = {product, n};
+			// update list and update index
 			customer.shoppingList[customer.index++] = shopList;
 			
 		}
@@ -200,16 +224,17 @@ struct Customer createShoppingList(char *csvfile){
 
 void printShop(struct Shop s)
 {
+	printf("\n\n-----------------\nSHOP\n-------------------\n");
 	printf("Shop has %.2f in cash\n", s.cash);
-	// addded for readablity
-	printf("\n");
+	
+	printf("\n-------------------\n");
 	
 	// going into shop and accessing each individual product
 	for (int i = 0; i < s.index; i++)
 	{
 		printProduct(s.stock[i].product);
 		printf("The shop has %d of the above\n", s.stock[i].quantity);
-		printf("\n");
+		printf("\n\n");
 	}
 }
 
@@ -231,7 +256,7 @@ int main(void)
 	// printCustomer(dominic);
 	
 	struct Shop shop = createAndStockShop();
-	//printShop(shop);
+	printShop(shop);
 	
 	struct Customer customer = createShoppingList("order1.csv");
 	printCustomer(customer);
